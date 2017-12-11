4c49246565e9d7a3e0ae2cf7c8bf1adafd013af6
Jesse Hammond <jessethammond@gmail.com>
2017-12-11 17:14:22 -0500

    added cards for index page

--- Gemfile
+++ Gemfile
@@ -18,6 +18,8 @@ gem 'simple_form'
 gem 'uglifier'
 gem 'cloudinary'
 gem 'carrierwave', '~> 0.11.2'
+gem "mini_magick"
+
 
 group :development, :test do
   gem 'pry-byebug'
--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,4 +1,9 @@
 # encoding: utf-8
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
+
+  def is_landscape? picture
+    image = MiniMagick::Image.open(picture.path)
+    image[:width] > image[:height]
+  end
 end
