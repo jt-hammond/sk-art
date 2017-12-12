2ce5d26b62863875375d0bc1e883993b51dfb49e
Jesse Hammond <jessethammond@gmail.com>
2017-12-12 13:44:46 -0500

    seed and mode update

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,3 +1,10 @@
 class Artwork < ApplicationRecord
   mount_uploader :photo, PhotoUploader
+
+  CATEGORY = ['stones', '11 X 14', 'Van Gogh', '44 X 44', '20 X 26']
+
+  validates :title, presence: true
+  validates :medium, presence: true
+  validates :category, :inclusion => { :in => CATEGORY }
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,257 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Artwork.delete_all
 
-Artwork.create(
-  title: ''
-  category: ''
-  medium: ''
-  photo: ''
-  sold: ''
-)
+
+attributes = [
+  [{
+    title: '',
+    category: '',
+    medium: '',
+    photo: '',
+    sold: ''
+    }, "app/assets/images/IMG_0371.jpg" ],
+  [{
+    title: '',
+    category: '',
+    medium: '',
+    photo: '',
+    sold: ''
+    }, "app/assets/images/IMG_0376.jpg" ],
+  [{
+    title: '',
+    category: '',
+    medium: '',
+    photo: '',
+    sold: ''
+    }, "app/assets/images/IMG_0383.jpg" ],
+  [{
+    title: '',
+    category: '',
+    medium: '',
+    photo: '',
+    sold: ''
+    }, "app/assets/images/IMG_0387.jpg" ],
+  [{
+    title: '',
+    category: '',
+    medium: '',
+    photo: '',
+    sold: ''
+    }, "app/assets/images/IMG_0391.jpg" ],
+  [{
+    title: '',
+    category: '',
+    medium: '',
+    photo: '',
+    sold: ''
+    }, "app/assets/images/IMG_0396.jpg" ],
+  [{
+    title: '',
+    category: '',
+    medium: '',
+    photo: '',
+    sold: ''
+    }, "app/assets/images/IMG_0398.jpg" ],
+  [{
+    title: '',
+    category: '',
+    medium: '',
+    photo: '',
+    sold: ''
+    }, "app/assets/images/IMG_0409.jpg" ],
+  [{
+    title: '',
+    category: '',
+    medium: '',
+    photo: '',
+    sold: ''
+    }, "app/assets/images/IMG_0421.jpg" ],
+  [{
+    title: '',
+    category: '',
+    medium: '',
+    photo: '',
+    sold: ''
+    }, "app/assets/images/IMG_0432.jpg" ],
+    {
+    title: '',
+    category: '',
+    medium: '',
+    photo: '',
+    sold: ''
+    }, "app/assets/images/IMG_0438.jpg" ],
+    {
+    title: '',
+    category: '',
+    medium: '',
+    photo: '',
+    sold: ''
+    }, "app/assets/images/IMG_0441.jpg" ],
+    {
+    title: '',
+    category: '',
+    medium: '',
+    photo: '',
+    sold: ''
+    }, "app/assets/images/IMG_0444.jpg" ],
+    {
+    title: '',
+    category: '',
+    medium: '',
+    photo: '',
+    sold: ''
+    }, "app/assets/images/IMG_0449.jpg" ],
+    {
+    title: '',
+    category: '',
+    medium: '',
+    photo: '',
+    sold: ''
+    }, "app/assets/images/IMG_0455.jpg" ],
+    {
+    title: '',
+    category: '',
+    medium: '',
+    photo: '',
+    sold: ''
+    }, "app/assets/images/IMG_0460.jpg" ],
+    {
+    title: '',
+    category: '',
+    medium: '',
+    photo: '',
+    sold: ''
+    }, "app/assets/images/IMG_0465.jpg" ],
+    {
+    title: '',
+    category: '',
+    medium: '',
+    photo: '',
+    sold: ''
+    }, "app/assets/images/IMG_0469.jpg" ],
+    {
+    title: '',
+    category: '',
+    medium: '',
+    photo: '',
+    sold: ''
+    }, "app/assets/images/IMG_0475.jpg" ],
+    {
+    title: '',
+    category: '',
+    medium: '',
+    photo: '',
+    sold: ''
+    }, "app/assets/images/IMG_0480.jpg" ],
+    {
+    title: '',
+    category: '',
+    medium: '',
+    photo: '',
+    sold: ''
+    }, "app/assets/images/IMG_0489.jpg" ],
+    {
+    title: '',
+    category: '',
+    medium: '',
+    photo: '',
+    sold: ''
+    }, "app/assets/images/IMG_0495.jpg" ],
+    {
+    title: '',
+    category: '',
+    medium: '',
+    photo: '',
+    sold: ''
+    }, "app/assets/images/IMG_0499.jpg" ],
+    {
+    title: '',
+    category: '',
+    medium: '',
+    photo: '',
+    sold: ''
+    }, "app/assets/images/IMG_0501.jpg" ],
+    {
+    title: '',
+    category: '',
+    medium: '',
+    photo: '',
+    sold: ''
+    }, "app/assets/images/IMG_0507.jpg" ],
+    {
+    title: '',
+    category: '',
+    medium: '',
+    photo: '',
+    sold: ''
+    }, "app/assets/images/IMG_0514.jpg" ],
+    {
+    title: '',
+    category: '',
+    medium: '',
+    photo: '',
+    sold: ''
+    }, "app/assets/images/IMG_0517.jpg" ],
+    {
+    title: '',
+    category: '',
+    medium: '',
+    photo: '',
+    sold: ''
+    }, "app/assets/images/IMG_0525.jpg" ],
+    {
+    title: '',
+    category: '',
+    medium: '',
+    photo: '',
+    sold: ''
+    }, "app/assets/images/IMG_0535.jpg" ],
+    {
+    title: '',
+    category: '',
+    medium: '',
+    photo: '',
+    sold: ''
+    }, "app/assets/images/IMG_0540.jpg" ],
+    {
+    title: '',
+    category: '',
+    medium: '',
+    photo: '',
+    sold: ''
+    }, "app/assets/images/IMG_0549.jpg" ],
+    {
+    title: '',
+    category: '',
+    medium: '',
+    photo: '',
+    sold: ''
+    }, "app/assets/images/IMG_0559.jpg" ],
+    {
+    title: '',
+    category: '',
+    medium: '',
+    photo: '',
+    sold: ''
+    }, "app/assets/images/IMG_0570.jpg" ],
+    {
+    title: '',
+    category: '',
+    medium: '',
+    photo: '',
+    sold: ''
+    }, "app/assets/images/nomad.jpg" ],
+
+  ]
+
+
+attributes.each do |attr|
+  artwork = Artwork.new(attr[0])
+  artwork.save!
+  artwork.photo = open(attr[1])
+end
+
+puts "Finished!"
