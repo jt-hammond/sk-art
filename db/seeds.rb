0c069472574a94d6b917483b817db88c92a07d6f
Jesse Hammond <jessethammond@gmail.com>
2017-12-12 16:18:18 -0500

    Photos finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,259 +3,293 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   movies = Movie.create([[{ name: 'Star Wars'
 #   Character.create(name: 'Luke', movie: movies.first)
 puts "Cleaning database..."
 Artwork.delete_all
 
 
 attributes = [
-  [{
-    title: '',
-    category: '',
-    medium: '',
-    photo: '',
-    sold: ''
+  [[{
+    title: 'Turtles',
+    category: '11 X 14',
+    medium: 'Watercolor',
+    price: '300',
+    sold: false
     }, "app/assets/images/IMG_0371.jpg" ],
-  [{
-    title: '',
-    category: '',
-    medium: '',
-    photo: '',
-    sold: ''
+  [[{
+    title: 'Three',
+    category: '11 X 14',
+    medium: 'Watercolor & Ink',
+    price: '300',
+    sold: false
     }, "app/assets/images/IMG_0376.jpg" ],
-  [{
-    title: '',
-    category: '',
-    medium: '',
-    photo: '',
-    sold: ''
+  [[{
+    title: 'Sundown',
+    category: '11 X 14',
+    medium: 'Watercolor & Ink',
+    price: '300',
+    sold: false
     }, "app/assets/images/IMG_0383.jpg" ],
-  [{
-    title: '',
-    category: '',
-    medium: '',
-    photo: '',
-    sold: ''
+  [[{
+    title: 'Somewhere There',
+    category: '11 X 14',
+    medium: 'Watercolor & Ink',
+    price: '300',
+    sold: false
     }, "app/assets/images/IMG_0387.jpg" ],
-  [{
-    title: '',
-    category: '',
-    medium: '',
-    photo: '',
-    sold: ''
+  [[{
+    title: 'Fairy Tears',
+    category: '11 X 14',
+    medium: 'Watercolor & Ink',
+    price: '300',
+    sold: false
     }, "app/assets/images/IMG_0391.jpg" ],
-  [{
-    title: '',
-    category: '',
-    medium: '',
-    photo: '',
-    sold: ''
+  [[{
+    title: 'Echos of the Walls',
+    category: '11 X 14',
+    medium: 'Ink',
+    price: '300',
+    sold: false
     }, "app/assets/images/IMG_0396.jpg" ],
-  [{
-    title: '',
-    category: '',
-    medium: '',
-    photo: '',
-    sold: ''
+  [[{
+    title: 'Vibrations',
+    category: '11 X 14',
+    medium: 'Watercolor',
+    price: '300',
+    sold: false''
     }, "app/assets/images/IMG_0398.jpg" ],
-  [{
-    title: '',
-    category: '',
-    medium: '',
-    photo: '',
-    sold: ''
+  [[{
+    title: 'Kiss 1',
+    category: '11 X 14',
+    medium: 'Watercolor',
+    price: '300',
+    sold: false
     }, "app/assets/images/IMG_0409.jpg" ],
   [{
-    title: '',
-    category: '',
-    medium: '',
-    photo: '',
-    sold: ''
+    title: 'Kiss 2',
+    category: '11 X 14',
+    medium: 'Watercolor & Ink',
+    price: '300',
+    sold: false
     }, "app/assets/images/IMG_0421.jpg" ],
   [{
-    title: '',
-    category: '',
-    medium: '',
-    photo: '',
-    sold: ''
+    title: 'Always Smiling',
+    category: '11 X 14',
+    medium: 'Watercolor',
+    price: '300',
+    sold: true
     }, "app/assets/images/IMG_0432.jpg" ],
-    {
+  [{
     title: '',
-    category: '',
-    medium: '',
-    photo: '',
-    sold: ''
+    category: '11 X 14',
+    medium: 'Watercolor',
+    price: '300',
+    sold: false
     }, "app/assets/images/IMG_0438.jpg" ],
-    {
-    title: '',
-    category: '',
-    medium: '',
-    photo: '',
-    sold: ''
+  [{
+    title: 'Hidden Memories',
+    category: '11 X 14',
+    medium: 'Watercolor & Ink',
+    price: '400',
+    sold: false
     }, "app/assets/images/IMG_0441.jpg" ],
-    {
-    title: '',
-    category: '',
-    medium: '',
-    photo: '',
-    sold: ''
+  [{
+    title: 'Legretha',
+    category: '11 X 14',
+    medium: 'Watercolor & Ink',
+    price: '400',
+    sold: false
     }, "app/assets/images/IMG_0444.jpg" ],
-    {
-    title: '',
-    category: '',
-    medium: '',
-    photo: '',
-    sold: ''
+  [{
+    title: 'Choral',
+    category: '11 X 14',
+    medium: 'Acrylic',
+    price: '400',
+    sold: false
     }, "app/assets/images/IMG_0449.jpg" ],
-    {
-    title: '',
-    category: '',
-    medium: '',
-    photo: '',
-    sold: ''
+  [{
+    title: 'Rangvarangi',
+    category: '11 X 14',
+    medium: 'Watercolor & Ink',
+    price: '400',
+    sold: false
     }, "app/assets/images/IMG_0455.jpg" ],
-    {
-    title: '',
-    category: '',
-    medium: '',
-    photo: '',
-    sold: ''
+  [{
+    title: 'Hiding from the World',
+    category: '11 X 14',
+    medium: 'Acrylic',
+    price: '400',
+    sold: false
     }, "app/assets/images/IMG_0460.jpg" ],
-    {
-    title: '',
-    category: '',
-    medium: '',
-    photo: '',
-    sold: ''
+  [{
+    title: 'Meditation',
+    category: '11 X 14',
+    medium: 'Acrylic & Pencil',
+    price: '400',
+    sold: false
     }, "app/assets/images/IMG_0465.jpg" ],
-    {
-    title: '',
-    category: '',
-    medium: '',
-    photo: '',
-    sold: ''
+  [{
+    title: 'Strange Magic',
+    category: '11 X 14',
+    medium: 'Acrylic & Watercolor',
+    price: '400',
+    sold: false
     }, "app/assets/images/IMG_0469.jpg" ],
-    {
-    title: '',
-    category: '',
-    medium: '',
-    photo: '',
-    sold: ''
+  [{
+    title: 'Gypsie Mama',
+    category: '11 X 14',
+    medium: 'Ink',
+    price: '300',
+    sold: false
     }, "app/assets/images/IMG_0475.jpg" ],
-    {
-    title: '',
-    category: '',
-    medium: '',
-    photo: '',
-    sold: ''
+  [{
+    title: 'Life Travels',
+    category: '11 X 14',
+    medium: 'Acrylic',
+    price: '400',
+    sold: false
     }, "app/assets/images/IMG_0480.jpg" ],
-    {
-    title: '',
-    category: '',
-    medium: '',
-    photo: '',
-    sold: ''
+  [{
+    title: 'Hint of Happiness',
+    category: '11 X 14',
+    medium: 'Watercolor',
+    price: '400',
+    sold: false
     }, "app/assets/images/IMG_0489.jpg" ],
-    {
-    title: '',
-    category: '',
-    medium: '',
-    photo: '',
-    sold: ''
+  [{
+    title: "Tree's Revelation",
+    category: '11 X 14',
+    medium: 'Watercolor',
+    price: '400',
+    sold: false
     }, "app/assets/images/IMG_0495.jpg" ],
-    {
-    title: '',
-    category: '',
-    medium: '',
-    photo: '',
-    sold: ''
+  [{
+    title: 'Waiting by the Waterfall',
+    category: '11 X 14',
+    medium: 'Watercolor & Ink',
+    price: '400',
+    sold: false
     }, "app/assets/images/IMG_0499.jpg" ],
-    {
-    title: '',
-    category: '',
-    medium: '',
-    photo: '',
-    sold: ''
+  [{
+    title: 'Billie',
+    category: '11 X 14',
+    medium: 'Watercolor & Ink',
+    price: '400',
+    sold: false
     }, "app/assets/images/IMG_0501.jpg" ],
-    {
-    title: '',
-    category: '',
-    medium: '',
-    photo: '',
-    sold: ''
+  [{
+    title: 'Exchanging Words',
+    category: '11 X 14',
+    medium: 'Watercolor & Ink',
+    price: '400',
+    sold: false
     }, "app/assets/images/IMG_0507.jpg" ],
-    {
-    title: '',
-    category: '',
-    medium: '',
-    photo: '',
-    sold: ''
+  [{
+    title: 'Attraction',
+    category: '11 X 14',
+    medium: 'Watercolor & Ink',
+    price: '400',
+    sold: false
     }, "app/assets/images/IMG_0514.jpg" ],
-    {
-    title: '',
-    category: '',
-    medium: '',
-    photo: '',
-    sold: ''
+  [{
+    title: 'Rebecca',
+    category: '20 X 26',
+    medium: 'Acrylic',
+    price: '575',
+    sold: false
     }, "app/assets/images/IMG_0517.jpg" ],
-    {
-    title: '',
-    category: '',
-    medium: '',
-    photo: '',
-    sold: ''
+  [{
+    title: 'Scars of Chaos',
+    category: '20 X 26',
+    medium: 'Watercolor',
+    price: '575',
+    sold: false
     }, "app/assets/images/IMG_0525.jpg" ],
-    {
-    title: '',
-    category: '',
-    medium: '',
-    photo: '',
-    sold: ''
+  [{
+    title: 'I Love You the Most Number 2',
+    category: '20 X 26',
+    medium: 'Watercolor & Ink',
+    price: '575',
+    sold: false
     }, "app/assets/images/IMG_0535.jpg" ],
-    {
-    title: '',
-    category: '',
-    medium: '',
-    photo: '',
-    sold: ''
+  [{
+    title: 'Woman with Brown Hair',
+    category: '18 X 24',
+    medium: 'Acrylic on Clayboard',
+    price: '400',
+    sold: false
     }, "app/assets/images/IMG_0540.jpg" ],
-    {
-    title: '',
-    category: '',
-    medium: '',
-    photo: '',
-    sold: ''
+  [{
+    title: 'I Love You the Most Number 1',
+    category: '20 X 26',
+    medium: 'Watercolor & Ink',
+    price: '575',
+    sold: false
     }, "app/assets/images/IMG_0549.jpg" ],
-    {
-    title: '',
-    category: '',
-    medium: '',
-    photo: '',
-    sold: ''
+  [{
+    title: 'Jubilation',
+    category: '20 X 26',
+    medium: 'Acrylic',
+    price: '575',
+    sold: false
     }, "app/assets/images/IMG_0559.jpg" ],
-    {
-    title: '',
-    category: '',
-    medium: '',
-    photo: '',
-    sold: ''
+  [{
+    title: 'Amorphis',
+    category: '20 X 26',
+    medium: 'Watercolor & Ink',
+    price: '575',
+    sold: false
     }, "app/assets/images/IMG_0570.jpg" ],
-    {
-    title: '',
-    category: '',
-    medium: '',
-    photo: '',
-    sold: ''
+  [{
+    title: 'The Awareness of Life and Time',
+    category: '20 X 26',
+    medium: 'Acrylic',
+    sold: false
+    price: '575',
+    }, "app/assets/images/IMG_0578.jpg" ],
+  [{
+    title: 'Nomad',
+    category: '11 X 14',
+    medium: 'Indian Ink & Watercolor',
+    sold: false
+    price: '300',
     }, "app/assets/images/nomad.jpg" ],
-
+  [{
+    title: 'Jade',
+    category: '11 X 14',
+    medium: 'Watercolor',
+    sold: false
+    price: '300',
+    } , "app/assets/images/IMG_0342.jpg" ]
+  [{
+    title: 'Pure Bliss',
+    category: '11 X 14',
+    medium: 'Ink',
+    sold: false
+    price: '300',
+    } , "app/assets/images/IMG_0366.jpg" ]
+  [{
+    title: 'Yearning',
+    category: '11 X 14',
+    medium: 'Watercolor',
+    sold: false
+    price: '300',
+    } , "app/assets/images/IMG_0363.jpg" ]
+  [{
+    title: 'Three Friends',
+    category: '11 X 14',
+    medium: 'Watercolor & Ink',
+    sold: false
+    price: '300',
+    } , "app/assets/images/IMG_0401.jpg" ]
   ]
 
 
 attributes.each do |attr|
   artwork = Artwork.new(attr[0])
+  artwork.photo = Rails.root.join(attr[1]).open
   artwork.save!
-  artwork.photo = open(attr[1])
 end
 
 puts "Finished!"
