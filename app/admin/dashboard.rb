57000c17227afcf14817b74adc953cf27ce8f3ba
Jesse Hammond <jessethammond@gmail.com>
2017-12-18 09:38:51 -0500

    Ready to merge

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -12,22 +12,22 @@ ActiveAdmin.register_page "Dashboard" do
 
     # Here is an example of a simple dashboard with columns and panels.
     #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    columns do
+      column do
+        panel "Recent Artworks" do
+          ul do
+            Artwork.last(5).map do |artwork|
+              li link_to(artwork.title, admin_artwork_path(artwork))
+            end
+          end
+        end
+      end
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
+      column do
+        panel "Welcome" do
+          para "Welcome to S. K. Hammond Art."
+        end
+      end
+    end
   end # content
 end
